2ccfa682e7bb84c66e92124766ed1eb1467f95e9
kolento <515835543@qq.com>
2023-11-17 16:28:51 +0800

    功能更新
    Signed-off-by: kolento <515835543@qq.com>

--- douyin.js
+++ douyin.js
@@ -1,18 +1,40 @@
 const puppeteer = require('puppeteer');
 
-console.log(123)
 (async () => {
-  // Launch the browser and open a new blank page
-  // const browser = await puppeteer.launch();
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
+  let room = '';
+  // 帐号1
+  const browser1 = await puppeteer.launch({headless: false});
+  const page1 = await browser1.newPage();
+  await page1.goto('https://www.baidu.com/');
+  await page1.setViewport({width: 1920, height: 1080});
 
-  // Navigate the page to a URL
-  await page.goto('https://www.baidu.com/');
+  let login1 = await page1.$("#s-top-loginbtn");
+  console.log('login1',login1)
+  login1.click();
 
-  // Set screen size
-  await page.setViewport({width: 1080, height: 1024});
+  setTimeout(async() => {
+    //进行登录
+    let userName1 = await page1.$("#TANGRAM__PSP_11__userName");
+    let password1 = await page1.$("#TANGRAM__PSP_11__password");
+    console.log('userName1',userName1)
+
+    await page1.$eval('#TANGRAM__PSP_11__userName', el => el.value = 'testtesttest');
+    await page1.$eval('#TANGRAM__PSP_11__password', el => el.value = 'testtesttest');
+    // userName1.value='test';
+    // password1.val='test';
+  }, 1000);
 
 
-  // await browser.close();
+
+
+  // 帐号2
+  const browser2 = await puppeteer.launch({headless: false});
+  const page2 = await browser2.newPage();
+  await page2.goto('https://www.baidu.com/');
+  await page2.setViewport({width: 1920, height: 1024});
+
+  let login2 = await page2.$("#s-top-loginbtn");
+  login2.click();
+
+
 })();
